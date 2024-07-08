a82daee0a20fd862d43b297abd398c3af0cfd2bb
Attila <attila.abreu@leste.com>
2024-07-08 16:42:27 -0300

    chore: Update PublicarVaga.vue with form fields and labels

--- src/components/views/PublicarVaga.vue
+++ src/components/views/PublicarVaga.vue
@@ -1,9 +1,66 @@
 <template>
-    <div>
-      <h1>Publicar Vaga</h1>
+  <div class="container py-4">
+    <div class="row">
+      <div class="col">
+        <h4>Aresente a sua vaga para milhares de profissionais e de graça</h4>
+      </div>
     </div>
+
+    <div class="row mt-3">
+      <div class="col">
+        <label class="form-label">Título da vaga</label>
+        <input type="text" class="form-control" />
+        <div class="form-text">Por exemplo: Programador JavaScript e VueJS.</div>
+      </div>
+    </div>
+
+    <div class="row mt-3">
+      <div class="col">
+        <label class="form-label">Desrição</label>
+        <textarea type="text" class="form-control"></textarea>
+        <div class="form-text">Informe os detalhes da vaga.</div>
+      </div>
+    </div>
+
+    <div class="row mt-3">
+      <div class="col">
+        <label class="form-label">Título da vaga</label>
+        <input type="number" class="form-control" />
+        <div class="form-text">Informe o salário.</div>
+      </div>
+    </div>
+
+    <div class="row mt-3">
+      <div class="col">
+        <label class="form-label">Modalidade</label>
+        <select class="form-select">
+          <option value="presencial">Presencial</option>
+          <option value="home-office">Home Office</option>
+        </select>
+        <div class="form-text">Informe onde as atividades serão realizadas.</div>
+      </div>
+    </div>
+
+    <div class="row mt-3">
+      <div class="col">
+        <label class="form-label">Tipo</label>
+        <select class="form-select">
+          <option value="1">CLT</option>
+          <option value="2">PJ</option>
+        </select>
+        <div class="form-text">Informe o tipo de contratação.</div>
+      </div>
+    </div>
+
+    <div class="row mt-3">
+      <div class="col">
+        <button type="submit" class="btn bn-primary">Cadastrar</button>
+      </div>
+    </div>
+
+  </div>
 </template>
-  
+
 <script>
 
 
@@ -14,5 +71,4 @@ export default {
 </script>
 
 
-<style>
-</style>
+<style></style>
